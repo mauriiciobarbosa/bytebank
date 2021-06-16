60692ff962fadbeb6c15700db4f38915689e054a
mauricio.santana <mauriiciobarbosa@gmail.com>
2021-06-16 14:48:07 -0300

    ajusta bugs.

--- lib/main_2.dart
+++ lib/main_2.dart
@@ -6,30 +6,50 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TransferFormScreen(),
+      home: Scaffold(
+        body: TransferListScreen(),
+      ),
     );
   }
 }
 
-class TransferListScreen extends StatelessWidget {
+class TransferListScreen extends StatefulWidget {
+  final List<TransferData> _entries = [];
+
+  @override
+  State<StatefulWidget> createState() => TransferListScreenState();
+}
+
+class TransferListScreenState extends State<TransferListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tranferências'),
       ),
+      body: ListView.builder(
+        itemCount: widget._entries.length,
+        itemBuilder: (context, index) {
+          return TransferItem(widget._entries[index]);
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
+          final Future<TransferData?> future =
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return TransferFormScreen();
+          }));
           // do nothing
+
+          future.then((newTransfer) {
+            if (newTransfer != null) {
+              setState(() {
+                widget._entries.add(newTransfer);
+              });
+            }
+          });
         },
-      ),
-      body: Column(
-        children: [
-          TransferItem(TransferData(100.00, 101)),
-          TransferItem(TransferData(200.00, 102)),
-          TransferItem(TransferData(300.00, 103)),
-        ],
       ),
     );
   }
@@ -70,51 +90,73 @@ class TransferFormScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Criando Transferência')),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-            child: TextField(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TransferField(
               controller: accountNumberController,
-              style: TextStyle(fontSize: 16),
-              keyboardType: TextInputType.number,
               maxLength: 6,
-              decoration: InputDecoration(
-                labelText: 'Número da Conta',
-                hintText: '000000',
-              ),
+              label: 'Número da Conta',
+              hint: '000000',
+              icon: null,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-            child: TextField(
+            TransferField(
               controller: valueController,
-              style: TextStyle(fontSize: 16),
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'Valor',
-                  hintText: '0.00',
-                  icon: Icon(Icons.monetization_on)),
+              maxLength: 20,
+              label: 'Valor',
+              hint: '0.00',
+              icon: Icons.monetization_on,
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final int? accountNumber =
-                  int.tryParse(accountNumberController.text);
-              final double? value = double.tryParse(valueController.text);
+            ElevatedButton(
+              onPressed: () => _doTransfer(context),
+              child: Text('Confirmar'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
-              if (accountNumber != null && value != null) {
-                final newTransfer = TransferData(value, accountNumber);
+  void _doTransfer(BuildContext context) {
+    final int? accountNumber = int.tryParse(accountNumberController.text);
+    final double? value = double.tryParse(valueController.text);
 
-                FocusScope.of(context).unfocus();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('$newTransfer'))
-               );
-              }
-            },
-            child: Text('Confirmar'),
-          )
-        ],
+    if (accountNumber != null && value != null) {
+      final newTransfer = TransferData(value, accountNumber);
+
+      Navigator.pop(context, newTransfer);
+    }
+  }
+}
+
+class TransferField extends StatelessWidget {
+  final TextEditingController controller;
+  final int maxLength;
+  final String label;
+  final String hint;
+  final IconData? icon;
+
+  TransferField(
+      {required this.controller,
+      required this.maxLength,
+      required this.label,
+      required this.hint,
+      this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(fontSize: 16),
+        keyboardType: TextInputType.number,
+        maxLength: maxLength,
+        decoration: InputDecoration(
+          labelText: label,
+          hintText: hint,
+          icon: icon != null ? Icon(icon) : null,
+        ),
       ),
     );
   }
